13abb3cb16f637e6b528114161699bdf4f1b3b7d
Olabanji Andrew Okulenu <108607615+teflonRozay@users.noreply.github.com>
2022-10-25 19:18:23 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img
+        src="https://previews.123rf.com/images/stockbroker/stockbroker1507/stockbroker150705850/42307415-man-working-at-computer-in-contemporary-office.jpg"
+        alt="image"
+      />
+      <h1>Anthony Nicolas</h1>
+      <button id="btn_zuri">
+        <a href="https://twitter.com/IAMBANJIWILSON">Twitter Link</a>
+      </button>
+      <button id="">
+        <a href="https://training.zuri.team">Zuri Team</a>
+      </button>
+      <button>
+        <a href=" http://books.zuri.team">Zuri Books</a>
+      </button>
+      <button>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E">
+          Python Books
+        </a>
+      </button>
+      <button>
+        <a href="https://background.zuri.team">Background Checks For Code</a>
+      </button>
+      <button>
+        <a href="https://books.zuri.team/design-rules">Design Books</a>
+      </button>
+    </div>
+  );
+}
